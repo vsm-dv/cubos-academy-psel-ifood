f54dafbeea4876795b27240fd534d5f75c12bb5d
vsm-dv <valeria.mouraaa@gmail.com>
2021-08-10 20:41:04 -0300

    Desafio para converter o tamanho das camisas

---
+++ tamanho_das_camisas.js
@@ -0,0 +1,17 @@
+function solucao(tamanhoBr) {
+    if (tamanhoBr == "PP") {
+        console.log("XS");
+    }
+    else if (tamanhoBr == "P") {
+        console.log("S");
+    }
+    else if (tamanhoBr == "M") {
+        console.log("M");
+    }
+    else if (tamanhoBr == "G") {
+        console.log("L");
+    }
+    else {
+        console.log("XL");
+    }
+}
